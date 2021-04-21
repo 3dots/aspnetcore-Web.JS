3dots/aspnetcore-Web.JS

commit bfe0a0be6cae2605d4c1d030bb410f6506b3179d

Remove Entrypoint Invoker (#31769)
* Remove Entrypoint Invoker

* Ignore Samples .vscode config

* PR Feedback

* Update src/Components/Web.JS/src/Platform/Mono/MonoPlatform.ts

* Await callEntryPoint

* Check culture equality and display error

* Remove unnecessary comment

src/Boot.WebAssembly.ts
@@ -119,7 +119,7 @@ async function boot(options?: Partial<WebAssemblyStartOptions>): Promise<void> {
   }
 
   // Start up the application
-  platform.callEntryPoint(resourceLoader.bootConfig.entryAssembly);
+  await platform.callEntryPoint(resourceLoader.bootConfig.entryAssembly);
 }
 
 function invokeJSFromDotNet(callInfo: Pointer, arg0: any, arg1: any, arg2: any): any {

src/Platform/Mono/MonoPlatform.ts
@@ -54,16 +54,15 @@ export const monoPlatform: Platform = {
     });
   },
 
-  callEntryPoint: function callEntryPoint(assemblyName: string) {
-    // Instead of using Module.mono_call_assembly_entry_point, we have our own logic for invoking
-    // the entrypoint which adds support for async main.
-    // Currently we disregard the return value from the entrypoint, whether it's sync or async.
-    // In the future, we might want Blazor.start to return a Promise<Promise<value>>, where the
-    // outer promise reflects the startup process, and the inner one reflects the possibly-async
-    // .NET entrypoint method.
-    const invokeEntrypoint = bindStaticMethod('Microsoft.AspNetCore.Components.WebAssembly', 'Microsoft.AspNetCore.Components.WebAssembly.Hosting.EntrypointInvoker', 'InvokeEntrypoint');
-    // Note we're passing in null because passing arrays is problematic until https://github.com/mono/mono/issues/18245 is resolved.
-    invokeEntrypoint(assemblyName, null);
+  callEntryPoint: async function callEntryPoint(assemblyName: string) : Promise<any> {
+    const emptyArray = [ [ ] ];
+
+    try {
+      await BINDING.call_assembly_entry_point(assemblyName, emptyArray, "m")
+    } catch (error) {
+      console.error(error);
+      showErrorNotification();
+    }
   },
 
   toUint8Array: function toUint8Array(array: System_Array<any>): Uint8Array {

src/Platform/Mono/MonoTypes.ts
@@ -21,6 +21,7 @@ declare interface BINDING {
   mono_array_to_js_array<TInput, TOutput>(array: System_Array<TInput>) : Array<TOutput>;
   conv_string(dotnetString: System_String | null): string | null;
   bind_static_method(fqn: string, signature?: string): Function;
+  call_assembly_entry_point(assemblyName: string, args: any[], signature: any): Promise<any>;
   unbox_mono_obj(object: System_Object): any;
 }
 

src/Platform/Platform.ts
@@ -3,7 +3,7 @@ import { WebAssemblyResourceLoader } from './WebAssemblyResourceLoader';
 export interface Platform {
   start(resourceLoader: WebAssemblyResourceLoader): Promise<void>;
 
-  callEntryPoint(assemblyName: string): void;
+  callEntryPoint(assemblyName: string): Promise<any>;
 
   toUint8Array(array: System_Array<any>): Uint8Array;
 

commit 1e5db3e30f7372d179a3e78ef9fc51b1bb663803

Remove await for WASM `platform.callEntryPoint` (#31978)
## Description

Regression introduced via: #31769

* On Blazor Server, `Blazor.start` returns a promise that resolves when the .NET code starts up and the app becomes interactive
* On WebAssembly:
  * Prior to #31769, `Blazor.start` behaves the same as server (the promise resolves when the app becomes interactive)
  * Since #31769, `Blazor.start` returns a promise that doesn’t resolve until the .NET app exits, which typically means never, and hence prevents its usage for triggering post-.NET startup logic

This PR is meant to revert that specific bit of logic, so that we no longer await the `platform.callEntryPoint` function and thus `Blazor.start` returns a promise that resolves when the .NET code starts up and the app becomes interactive. This now matches the behavior from 6.0-preview3.

## Customer Impact
Without this PR we inadvertently introduce a breaking change where the `Blazor.start` promise doesn't resolve until the .NET app exits. This prevents its usage for triggering post-.NET startup logic.

## Regression?
- [x] Yes
- [ ] No

[If yes, specify the version the behavior has regressed from]: 

Regression from 6.0-preview3 
Regressed by: #31769

## Risk
- [ ] High
- [ ] Medium
- [x] Low

[Justify the selection above]
Returning the `await` logic to what it was prior to https://github.com/dotnet/aspnetcore/pull/31769 which was introduced in Preview 4.

## Verification
- [x] Manual (required)
- [x] Automated: https://github.com/dotnet/aspnetcore/pull/31997

## Packaging changes reviewed?
- [ ] Yes
- [ ] No
- [x] N/A

Addresses https://github.com/dotnet/aspnetcore/issues/31971

src/Boot.WebAssembly.ts
@@ -119,7 +119,7 @@ async function boot(options?: Partial<WebAssemblyStartOptions>): Promise<void> {
   }
 
   // Start up the application
-  await platform.callEntryPoint(resourceLoader.bootConfig.entryAssembly);
+  platform.callEntryPoint(resourceLoader.bootConfig.entryAssembly);
 }
 
 function invokeJSFromDotNet(callInfo: Pointer, arg0: any, arg1: any, arg2: any): any {